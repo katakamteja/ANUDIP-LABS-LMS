464729a4452f069b7246d719c7f1ba18a91c345a
teja <168896993+chinnuwithcode@users.noreply.github.com>
2024-07-18 19:42:34 +0530

    Add files via upload

---
+++ AboutYourSelf.java
@@ -0,0 +1,19 @@
+package anudip_LMS_LABS;
+
+public class AboutYourSelf {
+
+	public static void main(String[] args) {
+		// To display 10 sentences about myself
+		System.out.println("1.My name is katakam teja.");
+		System.out.println("2.Im from Hyderabad.");
+		System.out.println("3.Currently im pursuing MCA.");
+		System.out.println("4.I have one siblings.");
+		System.out.println("5.I belongs to nuclear family");
+		System.out.println("6.My hobbies are listening music and watching movies and series");
+		System.out.println("7.My sort term goal is to become a FULL STACK DEVELOPER");
+		System.out.println("8.My goal is get job in a MNC companies");
+		System.out.println("9.My long term goal is to be in a good position and upgrade my skills for better growth in career");
+		System.out.println("10.I am a quick learner.");
+		System.out.println("thank you thats all about me");
+	}
+}
---
+++ JVM.java
@@ -0,0 +1,23 @@
+package anudip_LMS_LABS;
+
+public class JVM {
+public static void main(String[] args)
+		{
+			//To display 15 sentences to describe the architecture of JVM
+			System.out.println("1.JVM stands for java virtual machine ");
+			System.out.println("2.It is a software-based engine");
+			System.out.println("3.Class loader is responsible for loading,linking.intialisation");
+			System.out.println("4.It is neccessary for running java bytecode applications");
+			System.out.println("5.Three different class loaders are boostrap,extention,application ");
+			System.out.println("6.Boostrap class loader is a root class loader");
+			System.out.println("7.The three subsections in JVM are: 1.Class loadeer /ln 2.Runtime data ideas,3.execution engine ");
+			System.out.println("8.Application class loader is a final classloader");
+			System.out.println("9.Runtime data areas include method area , heap area,stack area,pc register and native method stacks");
+			System.out.println("10.JVM helps programmers to right code in systematically");
+			System.out.println("11.Execution engine includes interpreter JIT compiler garbage collecter ");
+			System.out.println("12.It is responsible for loading file that has been written and compile into(.classfile)");
+			System.out.println("13.JVM is implemented through the JRE(Java Runtime Environment)");
+			System.out.println("14.Linking involves verification,preparation and reolving");
+			System.out.println("15.Execution engine includes interpreter");
+		}
+	}
