f5e17a9b093c6ea807ef86b47e9fcc12136974ed
teja <168896993+chinnuwithcode@users.noreply.github.com>
2024-07-18 20:24:03 +0530

    Add files via upload

---
+++ LeapYear.java
@@ -0,0 +1,19 @@
+package anudip_LMS_LABS;
+
+public class LeapYear {
+	// Write a program to check a year is leap year or not.
+	      public static void main(String [] args)
+	      {
+	    	  int year=2024;
+	    	  if(year%4 == 0)
+	    	  {
+	    		  System.out.println("It is a leap year");
+	    	  }
+	    	  else
+	    	  {
+	    		  System.out.println("It is not a leap year");
+	    	  }
+	      }
+	}
+
+
